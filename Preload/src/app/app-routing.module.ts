d2b0e7d6e7a90cde380c5d00cec1cb5d813d920a
oleksiyo <kozzzak@gmail.com>
2020-07-09 16:22:02 +0300

    load admin module if need

--- Preload/src/app/app-routing.module.ts
+++ Preload/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from './content/content/content.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin/admin.module').then(mod => mod.AdminModule),
+  },
+  {
+    path: '',
+    component: ContentComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
